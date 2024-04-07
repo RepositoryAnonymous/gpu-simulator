1a061970625d446b0dc4a9e8d32c30796f1c0fd6
RepositoryAnonymous <166107814+RepositoryAnonymous@users.noreply.github.com>
2024-04-07 17:18:28 +0800

    Update process_sass_dir.cpp

--- sass-split/process_sass_dir.cpp
+++ sass-split/process_sass_dir.cpp
@@ -43,6 +43,22 @@ bool isSassFile(const std::string &file) {
   return false;
 }
 
+// 2024.04.07 Start
+bool isMemoryFile(const std::string &file) {
+  const std::string mem_ext = ".mem";
+  const std::string split_mem_ext = "_block_";
+
+  if (file.size() >= mem_ext.size() &&
+      file.substr(file.size() - mem_ext.size()) == mem_ext) {
+    if (file.find(split_mem_ext) != std::string::npos) {
+      return false;
+    }
+    return true;
+  }
+  return false;
+}
+// 2024.04.07 End
+
 int main(int argc, char *argv[]) {
   if (argc != 3 || std::string(argv[1]) != "--dir") {
     std::cerr << "Usage: " << argv[0] << " --dir <directory_of_sass_files>\n";
@@ -77,6 +93,34 @@ int main(int argc, char *argv[]) {
     perror("");
     return EXIT_FAILURE;
   }
+
+  // 2024.04.07 Start
+  std::string memory_dir = argv[2] + std::string("/../memory_traces");
+  std::vector<std::string> memory_files;
+  if ((dir = opendir(memory_dir.c_str())) != NULL) {
+    while ((ent = readdir(dir)) != NULL) {
+      if (isMemoryFile(ent->d_name)) {
+        std::cout << "Found mem file: " << ent->d_name << std::endl;
+        memory_files.push_back(getFullPath(memory_dir, std::string(ent->d_name)));
+      } else {
+        if (strcmp(ent->d_name, ".") != 0 && strcmp(ent->d_name, "..") != 0) {
+          std::string filePath =
+              getFullPath(memory_dir, std::string(ent->d_name));
+
+          if (std::remove(filePath.c_str()) == 0) {
+            std::cout << "Deleted non-mem file: " << ent->d_name << std::endl;
+          } else {
+            std::cerr << "Error deleting file: " << ent->d_name << std::endl;
+          }
+        }
+      }
+    }
+    closedir(dir);
+  } else {
+    perror("");
+    return EXIT_FAILURE;
+  }
+  // 2024.04.07 End
 
   std::map<std::pair<int, int>, std::vector<std::string>> warp_content;
 
@@ -118,6 +162,55 @@ int main(int argc, char *argv[]) {
       f_open.close();
     }
   }
+
+  // 2024.04.07 Start
+  std::map<std::pair<int, int>, std::vector<std::string>> blk_content;
+  for (const auto &mem_file : memory_files) {
+
+    std::cout << "Processing " << mem_file << "\n";
+    std::ifstream file(mem_file);
+    std::string content((std::istreambuf_iterator<char>(file)),
+                        std::istreambuf_iterator<char>());
+    
+    std::istringstream iss(content);
+    
+    auto underscorePos = mem_file.find_last_of('_');
+    int kernel_id = std::stoi(mem_file.substr(
+        underscorePos + 1, mem_file.find(".mem") - underscorePos - 1));
+
+    std::string line_str;
+    int block_id;
+
+    while (std::getline(iss, line_str)) {
+      std::istringstream line_iss(line_str);
+      std::string block_id_str;
+      
+      if (std::getline(line_iss, block_id_str, ' ')) {
+        block_id = std::stoi(block_id_str, nullptr, 16);
+        std::string remaining_content((std::istreambuf_iterator<char>(line_iss)),
+                                       std::istreambuf_iterator<char>());
+        if (blk_content.find({kernel_id, block_id}) == blk_content.end()) {
+          blk_content[{kernel_id, block_id}] = std::vector<std::string>();
+        }
+        blk_content[{kernel_id, block_id}].push_back(remaining_content);
+      }
+    }
+
+    file.close();
+
+    for (auto &item : blk_content) {
+      std::string outputPath =
+          memory_dir + "/kernel_" + std::to_string(item.first.first) +
+          "_block_" + std::to_string(item.first.second) + ".mem";
+      std::ofstream f_open;
+      f_open.open(outputPath);
+      for (auto &line : item.second) {
+        f_open << line;
+      }
+      f_open.close();
+    }
+  }
+  // 2024.04.07 End
 
   return 0;
 }
