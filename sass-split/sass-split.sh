b2a5fb55d5335dac812f8e831362d4ace3c8aea5
RepositoryAnonymous <166107814+RepositoryAnonymous@users.noreply.github.com>
2024-04-07 17:37:34 +0800

    Update sass-split.sh

--- sass-split/sass-split.sh
+++ sass-split/sass-split.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-SEARCH_ROOT="/home/XXX/apps/OursTracesCollection"
+SEARCH_ROOT="../apps/OursTracesCollection"
 
 CMD="./process_sass_dir --dir"
 
 find "$SEARCH_ROOT" -type d -name "sass_traces" | while read dir; do
     echo "Processing directory: $dir"
     $CMD "$dir"
+    echo "Current Time: $(date)"
 done
